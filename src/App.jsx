622d3fb011dbd7e503bc46bdc0514398da23bdae
remzr <110097561+remzr@users.noreply.github.com>
2026-01-07 20:31:09 +0100

    Write header html and connect to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <h1 class="text-3x1 font-bold underline">
         Webshop
       </h1>
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return(
+        <header>
+                <div>
+                    <button>Icon</button>
+                </div>
+                <nav>
+                    <button>Home</button>
+                    <button>Shop</button>
+                    <button>Cart</button>
+                </nav>
+        </header>
+    )
+}
+
+export default Header;
