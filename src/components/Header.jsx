409fa200ac71750c418778ea40ce43524cad286c
remzr <110097561+remzr@users.noreply.github.com>
2026-01-08 23:14:03 +0100

    Add react icons: Not wworking with proper size yet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router";
+import { GiAbstract005 } from "react-icons/gi";
 
 function Header() {
     return(
         <header className="flex w-full justify-between px-8 py-4 bg-zinc-200">
-                <div className="w-15 flex-none">
-                    <Link to="/">Icon</Link>
+                <div className="w-20 flex-none">
+                    <Link className="w-full flex" to="/">
+                        <GiAbstract005 className="text-large color-green-200"/>
+                        <a className="font-default text-lg font-medium inline">Necrons</a>
+                    </Link>
                 </div>
-                <nav className="w-85">
+                <nav className="w-80">
                     <ul className="flex auto w-full justify-end gap-8">
                         <li className="w-full"><Link to="/" className="font-default text-lg hover:font-medium">Home</Link></li>
                         <li className="w-full"><Link to="/shop" className="font-default text-lg hover:font-medium">Shop</Link></li>
