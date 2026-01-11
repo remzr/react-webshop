3044e89456829b0a04491d34573b580439319012
remzr <110097561+remzr@users.noreply.github.com>
2026-01-11 21:29:33 +0100

    Add: Render items in cart/checkout screen

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,17 +3,33 @@ import Header from "../components/Header"
 
 function Cart({cartContent}) {
 
+  const cartList = cartContent.map(item =>
+
+    <li key={item.id} className="flex m-4 items-center bg-zinc-800 rounded-lg">
+      {cartContent.length > 0 && (
+      <>  
+        <img className="w-15 rounded-l-lg"src={item.image}></img>
+        <span className="pl-8 font-default text-zinc-200 text-lg">{item.amount}</span>
+        <span className="pl-8 font-default text-zinc-200 text-lg">{item.name}</span>
+      </>
+      )}
+    </li>
+  )
+
   return (
     <>
     <Header cartContent={cartContent} />
-    <main className="p-8 bg-zinc-200">
-      <h1 className="text-3x1 font-bold underline">
+    <main className="p-8 bg-black">
+      <h1 className="pb-8 font-default text-zinc-200 text-6xl font-bold">
         Shoppingcart
       </h1>
-      <p className="text-3x1 mb-8">
-        {cartContent.length}
-      </p>
-      <Button callToAction="Go to checkout"/>
+      <div className="flex w-full">
+        <ul className="w-2/3 pr-16">{cartList}</ul>
+        <aside>
+          <h2></h2>
+          <Button callToAction="Go to checkout"/>
+        </aside>
+      </div>
     </main>
     </>
   )
